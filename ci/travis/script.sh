c2c3123e8eb7c5cb63d65de595e1609ef458820c
exprmntr <exprmntr@gmail.com>
2017-11-07 11:00:12 +0300

    Update script.sh

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -1,10 +1,16 @@
 #!/bin/bash -ex
 
-if [ "${CB_BUILD_AGENT}" == 'clang-linux-x86_64-release-cuda' ]; then
+function install_cuda_linux()
+{
     wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1404-8-0-local-ga2_8.0.61-1_amd64-deb -O cuda-repo-ubuntu1404-8-0-local-ga2_8.0.61-1_amd64.deb
     sudo dpkg -i cuda-repo-ubuntu1404-8-0-local-ga2_8.0.61-1_amd64.deb
     sudo apt-get update
-    sudo apt-get install cuda
+    sudo apt-get install cuda    
+}
+
+
+if [ "${CB_BUILD_AGENT}" == 'clang-linux-x86_64-release-cuda' ]; then
+    install_cuda_linux;
     ./ya make --stat -T -r -j 1 catboost/cuda/app -DCUDA_ROOT=/usr/local/cuda-8.0;
     cp $(readlink -f catboost/cuda/app/cb_cuda) catboost-cuda-linux;
 fi
@@ -18,15 +24,17 @@ fi
 #     cp $(readlink -f catboost/app/catboost) catboost-linux;
 # fi
 
-# if [ "${CB_BUILD_AGENT}" == 'python2-linux-x86_64-release' ]; then
-#     cd catboost/python-package;
-#     python2 ./mk_wheel.py -j 1;
-# fi
+if [ "${CB_BUILD_AGENT}" == 'python2-linux-x86_64-release' ]; then
+     install_cuda_linux;
+     cd catboost/python-package;
+     python2 ./mk_wheel.py -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0;
+fi
 
-# if [ "${CB_BUILD_AGENT}" == 'python3-linux-x86_64-release' ]; then
-#     cd catboost/python-package;
-#     python3 ./mk_wheel.py -j 1;
-# fi
+if [ "${CB_BUILD_AGENT}" == 'python3-linux-x86_64-release' ]; then
+     install_cuda_linux;
+     cd catboost/python-package;
+     python3 ./mk_wheel.py -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0;
+fi
 
 # if [ "${CB_BUILD_AGENT}" == 'clang-darwin-x86_64-debug' ]; then
 #     ./ya make --stat -T -d -j 1 catboost/app catboost/pytest;
