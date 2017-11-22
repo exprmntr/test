35523653b2b787100e8a0c1b6e8d0c006e58102f
exprmntr <exprmntr@gmail.com>
2017-11-22 16:19:57 +0300

    Update script.sh

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -46,20 +46,20 @@ if [ "${CB_BUILD_AGENT}" == 'python36-linux-x86_64-release' ]; then
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'clang-darwin-x86_64-release' ]; then
-    ./ya make --stat -T -r -j 1 catboost/app;
+    ./ya make --stat -T -r -j 2 catboost/app;
     cp $(readlink catboost/app/catboost) catboost-darwin;
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'clang-darwin-x86_64-release-cuda' ]; then
     install_cuda_darwin;
-    ./ya make --stat -T -r -j 1 catboost/cuda/app -DCUDA_ROOT=/usr/local/cuda;
+    ./ya make --stat -T -r -j 2 catboost/cuda/app -DCUDA_ROOT=/usr/local/cuda;
     cp $(readlink catboost/cuda/app/catboost) catboost-cuda-darwin;
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'python2-darwin-x86_64-release' ]; then
      install_cuda_darwin;
      cd catboost/python-package;
-     python2.7 ./mk_wheel.py -T -j 1 -DCUDA_ROOT=/usr/local/cuda;
+     python2.7 ./mk_wheel.py -T -j 2 -DCUDA_ROOT=/usr/local/cuda;
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'python35-darwin-x86_64-release' ]; then
@@ -67,7 +67,7 @@ if [ "${CB_BUILD_AGENT}" == 'python35-darwin-x86_64-release' ]; then
      brew install pyenv;
      pyenv install 3.5.2;
      cd catboost/python-package;
-     $HOME/.pyenv/versions/3.5.2/bin/python3.5 ./mk_wheel.py -T -j 1 -DCUDA_ROOT=/usr/local/cuda;
+     $HOME/.pyenv/versions/3.5.2/bin/python3.5 ./mk_wheel.py -T -j 2 -DCUDA_ROOT=/usr/local/cuda;
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'python36-darwin-x86_64-release' ]; then
@@ -75,6 +75,6 @@ if [ "${CB_BUILD_AGENT}" == 'python36-darwin-x86_64-release' ]; then
      cd catboost/python-package;
      brew install pyenv;
      pyenv install 3.6.3;
-     $HOME/.pyenv/versions/3.6.3/bin/python3.6 ./mk_wheel.py -T -j 1 -DCUDA_ROOT=/usr/local/cuda;
+     $HOME/.pyenv/versions/3.6.3/bin/python3.6 ./mk_wheel.py -T -j 2 -DCUDA_ROOT=/usr/local/cuda;
 fi
 
