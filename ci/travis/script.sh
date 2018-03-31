170138c9ab62756f75882d59fb87447fc8b0f524
exprmntr <exprmntr@gmail.com>
2018-03-31 23:56:25 +0300

    Update script.sh

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -ex
 
+if [ "$TRAVIS_OS_NAME" = 'osx' ]; then
+    export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python2.7/site-packages
+fi
+
 function install_cuda_linux()
 {
     wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1404-8-0-local-ga2_8.0.61-1_amd64-deb -O cuda-repo-ubuntu1404-8-0-local-ga2_8.0.61-1_amd64.deb
@@ -8,86 +12,73 @@ function install_cuda_linux()
     sudo apt-get install cuda    
 }
 
-function install_cuda_darwin()
-{
-    if [ -f $HOME/.ya/cuda_8.0.61_mac.dmg ]; then
-        rm $HOME/.ya/cuda_8.0.61_mac.dmg
-    fi
-    if [ $(openssl dgst -md5 -hex $HOME/.ya/cuda_9.0.176_mac.dmg | awk '{print $2;}') != 19369a391a7475cace0f3c377aebbecb ]; then
-        rm $HOME/.ya/cuda_9.0.176_mac.dmg
-    fi
-    
-    if [ ! -f $HOME/.ya/cuda_9.0.176_mac.dmg ]; then
-        wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_mac-dmg -c -O cuda_9.0.176_mac.dmg
-        if [ $(openssl dgst -md5 -hex cuda_9.0.176_mac.dmg | awk '{print $2;}') == 19369a391a7475cace0f3c377aebbecb ]; then
-           mv cuda_9.0.176_mac.dmg $HOME/.ya/cuda_9.0.176_mac.dmg
-        else
-           exit 1
-        fi
-    fi
-    hdiutil attach $HOME/.ya/cuda_9.0.176_mac.dmg
-    sudo /Volumes/CUDAMacOSXInstaller//CUDAMacOSXInstaller.app/Contents/MacOS/CUDAMacOSXInstaller --accept-eula --no-window
-    # exit 0  # XXX
-}
 
 if [ "${CB_BUILD_AGENT}" == 'clang-linux-x86_64-release-cuda' ]; then
     install_cuda_linux;
-    ./ya make --stat -T -r -j 1 catboost/cuda/app -DCUDA_ROOT=/usr/local/cuda-8.0;
-    cp $(readlink -f catboost/cuda/app/cb_cuda) catboost-cuda-linux;
-fi
-
-
-if [ "${CB_BUILD_AGENT}" == 'clang-linux-x86_64-release' ]; then
-    ./ya make --stat -T -r -j 1 catboost/app;
-    cp $(readlink -f catboost/app/catboost) catboost-linux;
+    ./ya make --no-emit-status --stat -T -r -j 1 catboost/app -DCUDA_ROOT=/usr/local/cuda-8.0 -DNO_DEBUGINFO;
+    cp $(readlink -f catboost/app/catboost) catboost-cuda-linux;
+    python ci/webdav_upload.py catboost-cuda-linux
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'python2-linux-x86_64-release' ]; then
      install_cuda_linux;
      cd catboost/python-package;
-     python2 ./mk_wheel.py -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0;
+     python2 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0 ;
+     python ../../ci/webdav_upload.py *.whl
+fi
+
+if [ "${CB_BUILD_AGENT}" == 'python34-linux-x86_64-release' ]; then
+     ls /home/travis/virtualenv
+     ln -s /home/travis/virtualenv/python3.4.6/bin/python-config /home/travis/virtualenv/python3.4.6/bin/python3-config;
+     install_cuda_linux;
+     cd catboost/python-package;
+     python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.4.6/bin/python3-config;
+     python ../../ci/webdav_upload.py *.whl
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'python35-linux-x86_64-release' ]; then
+     ln -s /home/travis/virtualenv/python3.5.5/bin/python-config /home/travis/virtualenv/python3.5.5/bin/python3-config;
      install_cuda_linux;
      cd catboost/python-package;
-     python3 ./mk_wheel.py -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0;
+     python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.5.5/bin/python3-config;
+     python ../../ci/webdav_upload.py *.whl
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'python36-linux-x86_64-release' ]; then
+     ln -s /home/travis/virtualenv/python3.6.3/bin/python-config /home/travis/virtualenv/python3.6.3/bin/python3-config;
      install_cuda_linux;
      cd catboost/python-package;
-     python3 ./mk_wheel.py -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0;
+     python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.6.3/bin/python3-config;
+     python ../../ci/webdav_upload.py *.whl
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'clang-darwin-x86_64-release' ]; then
-    ./ya make --stat -T -r -j 2 catboost/app;
+    ./ya make --no-emit-status --stat -T -r -j 1 catboost/app;
     cp $(readlink catboost/app/catboost) catboost-darwin;
+    python ci/webdav_upload.py catboost-darwin
 fi
 
-if [ "${CB_BUILD_AGENT}" == 'clang-darwin-x86_64-release-cuda' ]; then
-    install_cuda_darwin;
-    ./ya make --stat -T -r -j 2 catboost/cuda/app -DCUDA_ROOT=/usr/local/cuda;
-    cp $(readlink catboost/cuda/app/catboost) catboost-cuda-darwin;
-fi
+if [ "${CB_BUILD_AGENT}" == 'R-clang-darwin-x86_64-release' ] || [ "${CB_BUILD_AGENT}" == 'R-clang-linux-x86_64-release' ]; then
+    cd catboost/R-package
 
-if [ "${CB_BUILD_AGENT}" == 'python2-darwin-x86_64-release' ]; then
-     install_cuda_darwin;
-     cd catboost/python-package;
-     python2.7 ./mk_wheel.py -T -j 2 -DCUDA_ROOT=/usr/local/cuda;
-fi
+    mkdir catboost
 
-if [ "${CB_BUILD_AGENT}" == 'python35-darwin-x86_64-release' ]; then
-     install_cuda_darwin;
-     pyenv install 3.5.2;
-     cd catboost/python-package;
-     $HOME/.pyenv/versions/3.5.2/bin/python3.5 ./mk_wheel.py -T -j 2 -DCUDA_ROOT=/usr/local/cuda -DPYTHON_CONFIG=$HOME/.pyenv/versions/3.5.2/bin/python3-config;
-fi
+    cp DESCRIPTION catboost
+    cp NAMESPACE catboost
+    cp README.md catboost
 
-if [ "${CB_BUILD_AGENT}" == 'python36-darwin-x86_64-release' ]; then
-     install_cuda_darwin;
-     cd catboost/python-package;
-     pyenv install 3.6.3;
-     $HOME/.pyenv/versions/3.6.3/bin/python3.6 ./mk_wheel.py -T -j 2 -DCUDA_ROOT=/usr/local/cuda -DPYTHON_CONFIG=$HOME/.pyenv/versions/3.6.3/bin/python3-config;
+    cp -r R catboost
+
+    cp -r inst catboost
+    cp -r man catboost
+    cp -r tests catboost
+
+    ../../ya make -r -T src
+
+    mkdir catboost/inst/libs
+    cp $(readlink src/libcatboostr.so) catboost/inst/libs
+
+    tar -cvzf catboost-R-$(uname).tgz catboost
+    python ../../ci/webdav_upload.py catboost-R-*.tgz
 fi
 
